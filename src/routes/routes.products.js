fe11ba568e2e11350d215f95a3aecbe1876753bc
patxi <patxiarana05@gmail.com>
2023-05-04 16:21:46 -0300

    prove move route

--- src/routes/routes.products.js
+++ src/routes/routes.products.js
@@ -1,7 +1,8 @@
 import  {Router} from 'express';
-import { getProducts, createProducts,updateProducts,deleteProducts } from '../controllers/product.controllers.js';
+import { /*getProducts,*/ createProducts,updateProducts,deleteProducts } from '../controllers/product.controllers.js';
 import { Signin,signUp } from '../controllers/Auth.controllers.js';
 import express from "express"
+import {products} from '../models/products.js'
 //cors config
 import cors from "cors"
 // cors module
@@ -9,10 +10,36 @@ const app = express()
 
 const router = Router()
 
-router.get('/products/', getProducts)
+//router.get('/products/', getProducts)
+
+router.get('/', async (req, res) => {  try{
+    const products1 = await products.findAll({
+    });
+    res.json(products1)
+}catch(error){
+    return res.status(500).json({message: error.message})
+}
+})
+
+
+
+
+
+
+
+
+
 router.post('/products/',createProducts)
 router.put('/products/:id',updateProducts)
 router.delete('/products/:id',deleteProducts)
+
+
+
+
+
+
+
+
 
 
 
